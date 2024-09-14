717ced4d34811208b1a7099a634b8bd612e7cd38
OmarGit99 <omarshaikh@outlook.com>
2024-09-14 19:37:47 +0530

    Added timer

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def is_game_running(game_process_name):
 CLIENT_ID = '1284264076578394282'
 rpc = Presence(CLIENT_ID)
 rpc.connect()
-
+start_time=time.time()
 while True:
     if is_game_running('sh3.exe'):  
-        rpc.update(state="Walking through a nightmare", details="Exploring Silent Hill", large_image="iconalt", large_text="I hate mirrors..")
+        rpc.update(state="Walking through a nightmare", details="Exploring Silent Hill", large_image="iconalt", large_text="I hate mirrors..", start=start_time)
     else:
         rpc.clear()  
     time.sleep(15)
